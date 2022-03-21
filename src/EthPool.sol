09f7fc7ef04efc17087c1017000b9071e00f1995
Rootul Patel <rootulp@gmail.com>
2022-03-20 20:56:19 -0400

    Use open-zeppelin Ownable

--- src/EthPool.sol
+++ src/EthPool.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-contract EthPool {
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
+contract EthPool is Ownable {
+
+    event RewardsDeposited(uint amount);
+    uint256 public balance;
+
+    constructor() {
+        balance = 0;
+    }
+
+    function depositRewards() external payable onlyOwner() {
+        balance += msg.value;
+        emit RewardsDeposited(msg.value);
+    }
 }
--- src/test/EthPool.t.sol
+++ src/test/EthPool.t.sol
@@ -2,11 +2,25 @@
 pragma solidity 0.8.13;
 
 import "ds-test/test.sol";
+import "../EthPool.sol";
 
 contract EthPoolTest is DSTest {
-    function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    EthPool ethPool;
+    address owner = 0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84;
+    // address alice = address(0xCAFE);
+    // address bob = address(0xBEEF);
+
+    function setUp() public {
+        ethPool = new EthPool();
+    }
+
+    function testOwner() public {
+        assertEq(owner, ethPool.owner());
+    }
+
+    function testOwnerDepositRewards() public {
+        ethPool.depositRewards{value: 1 ether}();
+        assertEq(ethPool.balance(), 1 ether);
     }
 }
