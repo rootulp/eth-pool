2056673d5b430a60dde06e075a1715a1e5173bbd
Rootul Patel <rootulp@gmail.com>
2022-03-21 23:25:53 -0400

    Make deposits internal
    Per https://github.com/rari-capital/solcurity/

--- src/EthPool.sol
+++ src/EthPool.sol
@@ -3,21 +3,24 @@ pragma solidity 0.8.13;
 
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
+/// @title EthPool provides a pool for users to deposit into in order to earn rewards in ETH
+/// @author Rootul Patel
+/// @notice You can use this contract to deposit ETH into. If a pool operator deposits rewards
+/// into the pool while you have funds in the pool, you will be distributed some of the rewards proportional to your share of the total pool. You may withdraw your entire balance at any time.
+/// @dev This is an unaudited contract and shouldn't be used in production
 contract EthPool is Ownable {
 
     // State
-    uint256 public totalBalance;
-    address[] public depositors;
+    address[] internal depositors;
     mapping(address => uint) public balances;
+    uint256 public totalBalance;
 
     // Events
     event DistributeReward(uint amount);
     event Deposit(address indexed from, uint amount);
     event Withdrawal(address indexed to, uint amount);
 
-    constructor() {
-        totalBalance = 0;
-    }
+    constructor() {} // no-op
 
     function deposit() external payable {
         require(msg.sender != address(0));
